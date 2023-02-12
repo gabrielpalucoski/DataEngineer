5b0c4cbbc4de8e4b21efeab4e1cb359693f4cd40
Gabriel Palucoski <30905542+gabrielpalucoski@users.noreply.github.com>
2023-02-12 17:27:06 -0500

    Create numpy.py

---
+++ numpy.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Create a Numpy array
+a = np.array([1, 2, 3, 4, 5])
+print("Array:", a)
+
+# Calculate the mean of the array
+mean = np.mean(a)
+print("Mean:", mean)
+
+# Add 100 to each element of the array
+b = a + 100
+print("Array + 100:", b)
